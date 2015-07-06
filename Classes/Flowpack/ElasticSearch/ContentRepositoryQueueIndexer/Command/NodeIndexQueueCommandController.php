808f0aab4cc48681525b38dc2f0370ad7c5314f8
Dominique Feyer <dfeyer@ttree.ch>
2015-07-06 22:30:13 +0200

    [TASK] Change licence to MIT

--- Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/Command/NodeIndexQueueCommandController.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/Command/NodeIndexQueueCommandController.php
@@ -1,16 +1,6 @@
 <?php
 namespace Flowpack\ElasticSearch\ContentRepositoryQueueIndexer\Command;
 
-/*                                                                                                       *
- * This script belongs to the TYPO3 Flow package "Flowpack.ElasticSearch.ContentRepositoryQueueIndexer". *
- *                                                                                                       *
- * It is free software; you can redistribute it and/or modify it under                                   *
- * the terms of the GNU Lesser General Public License, either version 3                                  *
- *  of the License, or (at your option) any later version.                                               *
- *                                                                                                       *
- * The TYPO3 project - inspiring people to share!                                                        *
- *                                                                                                       */
-
 use Doctrine\Common\Persistence\ObjectManager;
 use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Command\NodeIndexCommandController;
 use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Indexer\NodeIndexer;
--- Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/IndexingJob.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/IndexingJob.php
@@ -1,16 +1,6 @@
 <?php
 namespace Flowpack\ElasticSearch\ContentRepositoryQueueIndexer;
 
-/*                                                                                                       *
- * This script belongs to the TYPO3 Flow package "Flowpack.ElasticSearch.ContentRepositoryQueueIndexer". *
- *                                                                                                       *
- * It is free software; you can redistribute it and/or modify it under                                   *
- * the terms of the GNU Lesser General Public License, either version 3                                  *
- *  of the License, or (at your option) any later version.                                               *
- *                                                                                                       *
- * The TYPO3 project - inspiring people to share!                                                        *
- *                                                                                                       */
-
 use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Indexer\NodeIndexer;
 use Flowpack\ElasticSearch\ContentRepositoryAdaptor\LoggerInterface;
 use TYPO3\Flow\Annotations as Flow;
