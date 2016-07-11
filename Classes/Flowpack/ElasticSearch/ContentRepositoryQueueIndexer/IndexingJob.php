683174a266b4ee89d1f4dbfc49c708b20b514106
Dominique Feyer <dfeyer@ttree.ch>
2016-07-11 12:53:07 +0200

    TASK: Implement bulk indexing support

--- Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/IndexingJob.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/IndexingJob.php
@@ -17,122 +17,131 @@ use TYPO3\TYPO3CR\Domain\Service\ContextFactory;
 /**
  * ElasticSearch Indexing Job Interface
  */
-class IndexingJob implements JobInterface {
+class IndexingJob implements JobInterface
+{
 
-	/**
-	 * @var NodeIndexer
-	 * @Flow\Inject
-	 */
-	protected $nodeIndexer;
+    /**
+     * @var NodeIndexer
+     * @Flow\Inject
+     */
+    protected $nodeIndexer;
 
-	/**
-	 * @var NodeDataRepository
-	 * @Flow\Inject
-	 */
-	protected $nodeDataRepository;
+    /**
+     * @var NodeDataRepository
+     * @Flow\Inject
+     */
+    protected $nodeDataRepository;
 
-	/**
-	 * @var NodeFactory
-	 * @Flow\Inject
-	 */
-	protected $nodeFactory;
+    /**
+     * @var NodeFactory
+     * @Flow\Inject
+     */
+    protected $nodeFactory;
 
-	/**
-	 * @var ContextFactory
-	 * @Flow\Inject
-	 */
-	protected $contextFactory;
+    /**
+     * @var ContextFactory
+     * @Flow\Inject
+     */
+    protected $contextFactory;
 
-	/**
-	 * @var SystemLoggerInterface
-	 * @Flow\Inject
-	 */
-	protected $logger;
+    /**
+     * @var SystemLoggerInterface
+     * @Flow\Inject
+     */
+    protected $logger;
 
-	/**
-	 * @var string
-	 */
-	protected $identifier;
+    /**
+     * @var string
+     */
+    protected $identifier;
 
-	/**
-	 * @var string
-	 */
-	protected $workspaceName;
+    /**
+     * @var string
+     */
+    protected $workspaceName;
 
-	/**
-	 * @var string
-	 */
-	protected $indexPostfix;
+    /**
+     * @var string
+     */
+    protected $indexPostfix;
 
-	/**
-	 * @var array
-	 */
-	protected $nodes = [];
+    /**
+     * @var array
+     */
+    protected $nodes = [];
 
-	/**
-	 * @param string $indexPostfix
-	 * @param string $workspaceName
-	 * @param array $nodes
-	 */
-	public function __construct($indexPostfix, $workspaceName, array $nodes) {
-		$this->identifier = Algorithms::generateRandomString(24);
-		$this->workspaceName = $workspaceName;
-		$this->indexPostfix = $indexPostfix;
-		$this->nodes = $nodes;
-	}
+    /**
+     * @param string $indexPostfix
+     * @param string $workspaceName
+     * @param array $nodes
+     */
+    public function __construct($indexPostfix, $workspaceName, array $nodes)
+    {
+        $this->identifier = Algorithms::generateRandomString(24);
+        $this->workspaceName = $workspaceName;
+        $this->indexPostfix = $indexPostfix;
+        $this->nodes = $nodes;
+    }
 
-	/**
-	 * Execute the job
-	 * A job should finish itself after successful execution using the queue methods.
-	 *
-	 * @param QueueInterface $queue
-	 * @param Message $message The original message
-	 * @return boolean TRUE if the job was executed successfully and the message should be finished
-	 */
-	public function execute(QueueInterface $queue, Message $message) {
-		foreach ($this->nodes as $node) {
-			/** @var NodeData $nodeData */
-			$nodeData = $this->nodeDataRepository->findByIdentifier($node['nodeIdentifier']);
-			$context = $this->contextFactory->create([
-				'workspaceName' => $this->workspaceName,
-				'invisibleContentShown' => true,
-				'inaccessibleContentShown' => false,
-				'dimensions' => $node['dimensions']
-			]);
-			$currentNode = $this->nodeFactory->createFromNodeData($nodeData, $context);
+    /**
+     * Execute the job
+     * A job should finish itself after successful execution using the queue methods.
+     *
+     * @param QueueInterface $queue
+     * @param Message $message The original message
+     * @return boolean TRUE if the job was executed successfully and the message should be finished
+     */
+    public function execute(QueueInterface $queue, Message $message)
+    {
+        $this->nodeIndexer->withBulkProcessing(function () {
+            foreach ($this->nodes as $node) {
+                /** @var NodeData $nodeData */
+                $nodeData = $this->nodeDataRepository->findByIdentifier($node['nodeIdentifier']);
+                $context = $this->contextFactory->create([
+                    'workspaceName' => $this->workspaceName,
+                    'invisibleContentShown' => true,
+                    'inaccessibleContentShown' => false,
+                    'dimensions' => $node['dimensions']
+                ]);
+                $currentNode = $this->nodeFactory->createFromNodeData($nodeData, $context);
 
-			// Skip this iteration if the node can not be fetched from the current context
-			if (!$currentNode instanceof NodeInterface) {
-				$this->logger->log(sprintf('Node with identifier %s could not be processed', $node['nodeIdentifier']));
-				continue;
-			}
+                // Skip this iteration if the node can not be fetched from the current context
+                if (!$currentNode instanceof NodeInterface) {
+                    $this->logger->log(sprintf('Node with identifier %s could not be processed', $node['nodeIdentifier']));
+                    continue;
+                }
 
-			$this->nodeIndexer->setIndexNamePostfix($this->indexPostfix);
-			$this->logger->log(sprintf('Process indexing job for %s', $currentNode));
-			$this->nodeIndexer->indexNode($currentNode);
-		}
+                $this->nodeIndexer->setIndexNamePostfix($this->indexPostfix);
+                $this->logger->log(sprintf('Process indexing job for %s', $currentNode));
 
-		$this->nodeIndexer->flush();
+                $this->nodeIndexer->indexNode($currentNode);
 
-		return TRUE;
-	}
+            }
 
-	/**
-	 * Get an optional identifier for the job
-	 *
-	 * @return string A job identifier
-	 */
-	public function getIdentifier() {
-		return $this->identifier;
-	}
+            $this->nodeIndexer->flush();
+        });
 
-	/**
-	 * Get a readable label for the job
-	 *
-	 * @return string A label for the job
-	 */
-	public function getLabel() {
-		return sprintf('ElasticSearch Indexing Job (%s)', $this->getIdentifier());
-	}
+        return true;
+    }
+
+    /**
+     * Get an optional identifier for the job
+     *
+     * @return string A job identifier
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * Get a readable label for the job
+     *
+     * @return string A label for the job
+     */
+    public function getLabel()
+    {
+        return sprintf('ElasticSearch Indexing Job (%s)', $this->getIdentifier());
+    }
 
 }
