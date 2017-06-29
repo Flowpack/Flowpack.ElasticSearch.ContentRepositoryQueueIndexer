fc7a17908eb8c5f9874f525a876fbc2617a8a2da
Karsten Dambekalns <karsten@dambekalns.de>
2017-06-29 18:10:27 +0200

    TASK: Add `RemovalJob` to handle removal asynchronously
    This complements the async handling of node indexing.

--- Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/Indexer/NodeIndexer.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/Indexer/NodeIndexer.php
@@ -4,6 +4,7 @@ namespace Flowpack\ElasticSearch\ContentRepositoryQueueIndexer\Indexer;
 use Flowpack\ElasticSearch\ContentRepositoryAdaptor;
 use Flowpack\ElasticSearch\ContentRepositoryQueueIndexer\Command\NodeIndexQueueCommandController;
 use Flowpack\ElasticSearch\ContentRepositoryQueueIndexer\IndexingJob;
+use Flowpack\ElasticSearch\ContentRepositoryQueueIndexer\RemovalJob;
 use Flowpack\JobQueue\Common\Job\JobManager;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Persistence\PersistenceManagerInterface;
@@ -49,5 +50,24 @@ class NodeIndexer extends ContentRepositoryAdaptor\Indexer\NodeIndexer
             ]
         ]);
         $this->jobManager->queue(NodeIndexQueueCommandController::LIVE_QUEUE_NAME, $indexingJob);
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @param string|null $targetWorkspaceName In case indexing is triggered during publishing, a target workspace name will be passed in
+     */
+    public function removeNode(NodeInterface $node, $targetWorkspaceName = null)
+    {
+        if ($this->enableLiveAsyncIndexing !== true) {
+            parent::removeNode($node, $targetWorkspaceName);
+            return;
+        }
+        $removalJob = new RemovalJob($this->indexNamePostfix, $targetWorkspaceName, [
+            [
+                'nodeIdentifier' => $this->persistenceManager->getIdentifierByObject($node->getNodeData()),
+                'dimensions' => $node->getDimensions()
+            ]
+        ]);
+        $this->jobManager->queue(NodeIndexQueueCommandController::LIVE_QUEUE_NAME, $removalJob);
     }
 }
---
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryQueueIndexer/RemovalJob.php
@@ -0,0 +1,151 @@
+<?php
+namespace Flowpack\ElasticSearch\ContentRepositoryQueueIndexer;
+
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Indexer\NodeIndexer;
+use Flowpack\ElasticSearch\ContentRepositoryQueueIndexer\Domain\Repository\NodeDataRepository;
+use Flowpack\JobQueue\Common\Job\JobInterface;
+use Flowpack\JobQueue\Common\Queue\Message;
+use Flowpack\JobQueue\Common\Queue\QueueInterface;
+use Neos\ContentRepository\Domain\Model\NodeData;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Utility\Algorithms;
+use Neos\ContentRepository\Domain\Factory\NodeFactory;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Service\ContextFactoryInterface;
+
+/**
+ * Elasticsearch Node Removal Job
+ */
+class RemovalJob implements JobInterface
+{
+    use LoggerTrait;
+
+    /**
+     * @var NodeIndexer
+     * @Flow\Inject
+     */
+    protected $nodeIndexer;
+
+    /**
+     * @var NodeDataRepository
+     * @Flow\Inject
+     */
+    protected $nodeDataRepository;
+
+    /**
+     * @var NodeFactory
+     * @Flow\Inject
+     */
+    protected $nodeFactory;
+
+    /**
+     * @var ContextFactoryInterface
+     * @Flow\Inject
+     */
+    protected $contextFactory;
+
+    /**
+     * @var string
+     */
+    protected $identifier;
+
+    /**
+     * @var string
+     */
+    protected $targetWorkspaceName;
+
+    /**
+     * @var string
+     */
+    protected $indexPostfix;
+
+    /**
+     * @var array
+     */
+    protected $nodes = [];
+
+    /**
+     * @param string $indexPostfix
+     * @param string $targetWorkspaceName In case indexing is triggered during publishing, a target workspace name will be passed in
+     * @param array $nodes
+     */
+    public function __construct($indexPostfix, $targetWorkspaceName, array $nodes)
+    {
+        $this->identifier = Algorithms::generateRandomString(24);
+        $this->targetWorkspaceName = $targetWorkspaceName;
+        $this->indexPostfix = $indexPostfix;
+        $this->nodes = $nodes;
+    }
+
+    /**
+     * Execute the job removal of nodes.
+     *
+     * @param QueueInterface $queue
+     * @param Message $message The original message
+     * @return boolean TRUE if the job was executed successfully and the message should be finished
+     */
+    public function execute(QueueInterface $queue, Message $message)
+    {
+        $this->nodeIndexer->withBulkProcessing(function () {
+            $numberOfNodes = count($this->nodes);
+            $startTime = microtime(true);
+            foreach ($this->nodes as $node) {
+                /** @var NodeData $nodeData */
+                $nodeData = $this->nodeDataRepository->findByIdentifier($node['nodeIdentifier']);
+
+                // Skip this iteration if the nodedata can not be fetched (deleted node)
+                if (!$nodeData instanceof NodeData) {
+                    $this->log(sprintf('action=removal step=failed node=%s message="Node data could not be loaded"', $node['nodeIdentifier']));
+                    continue;
+                }
+
+                $context = $this->contextFactory->create([
+                    'workspaceName' => $this->targetWorkspaceName ?: $nodeData->getWorkspace()->getName(),
+                    'invisibleContentShown' => true,
+                    'removedContentShown' => true,
+                    'inaccessibleContentShown' => false,
+                    'dimensions' => $node['dimensions']
+                ]);
+                $currentNode = $this->nodeFactory->createFromNodeData($nodeData, $context);
+
+                // Skip this iteration if the node can not be fetched from the current context
+                if (!$currentNode instanceof NodeInterface) {
+                    $this->log(sprintf('action=removal step=failed node=%s message="Node could not be processed"', $node['nodeIdentifier']));
+                    continue;
+                }
+
+                $this->nodeIndexer->setIndexNamePostfix($this->indexPostfix);
+                $this->log(sprintf('action=removal step=started node=%s', $currentNode->getIdentifier()));
+
+                $this->nodeIndexer->removeNode($currentNode);
+            }
+
+            $this->nodeIndexer->flush();
+            $duration = microtime(true) - $startTime;
+            $rate = $numberOfNodes / $duration;
+            $this->log(sprintf('action=removal step=finished number_of_nodes=%d duration=%f nodes_per_second=%f', $numberOfNodes, $duration, $rate));
+        });
+
+        return true;
+    }
+
+    /**
+     * Get an optional identifier for the job
+     *
+     * @return string A job identifier
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * Get a readable label for the job
+     *
+     * @return string A label for the job
+     */
+    public function getLabel()
+    {
+        return sprintf('Elasticsearch Removal Job (%s)', $this->getIdentifier());
+    }
+}
