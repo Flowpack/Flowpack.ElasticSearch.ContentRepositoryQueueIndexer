9ebac69d513424e28ca57d3acd9e13b7137c272d
Karsten Dambekalns <karsten@dambekalns.de>
2018-05-30 11:59:56 +0200

    TASK: Downgrade severity once more

--- Classes/IndexingJob.php
+++ Classes/IndexingJob.php
@@ -30,7 +30,7 @@ class IndexingJob extends AbstractIndexingJob
 
                 // Skip this iteration if the nodedata can not be fetched (deleted node)
                 if (!$nodeData instanceof NodeData) {
-                    $this->log(sprintf('action=indexing step=failed node=%s message="Node data could not be loaded"', $node['identifier']), \LOG_WARNING);
+                    $this->log(sprintf('action=indexing step=skipped node=%s message="Node data could not be loaded"', $node['identifier']), \LOG_NOTICE);
                     continue;
                 }
 
