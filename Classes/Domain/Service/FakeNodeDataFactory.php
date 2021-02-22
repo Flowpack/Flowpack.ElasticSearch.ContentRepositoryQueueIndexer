25de2363c1ad7dbfc30175604764206469d04a73
Günther Jedenastik <guenther@webandco.com>
2021-02-22 11:47:01 +0100

    BUGFIX: Make sure persistence check is correct

--- Classes/Domain/Service/FakeNodeDataFactory.php
+++ Classes/Domain/Service/FakeNodeDataFactory.php
@@ -84,7 +84,7 @@ class FakeNodeDataFactory
         $nodeData->setRemoved(true);
 
         // Ensure, the fake-node is not persisted
-        if ($this->persistenceManager->isNewObject($nodeData)) {
+        if (false === $this->persistenceManager->isNewObject($nodeData)) {
             $this->persistenceManager->remove($nodeData);
         }
 
